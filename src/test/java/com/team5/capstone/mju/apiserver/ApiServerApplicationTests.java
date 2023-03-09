547282722025dd35d461f48bcb1885a446fcaaf2
terria1020 <jaehan1346@gmail.com>
2023-03-09 21:24:23 +0900

    feat: Spring Boot 기본 프로젝트 생성

---
+++ src/test/java/com/team5/capstone/mju/apiserver/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.capstone.mju.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
