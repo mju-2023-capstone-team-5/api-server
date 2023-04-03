1de472929f49a47bd668c393a9d960cb50561c63
terria1020 <jaehan1346@gmail.com>
2023-04-03 12:34:35 +0900

    feat: 주차장 정보를 가져오게 하는 서비스 메소드 추가

--- src/main/java/com/team5/capstone/mju/apiserver/web/service/ParkingLotService.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/service/ParkingLotService.java
@@ -1,7 +1,24 @@
 package com.team5.capstone.mju.apiserver.web.service;
 
+import com.team5.capstone.mju.apiserver.web.entity.ParkingLot;
+import com.team5.capstone.mju.apiserver.web.repository.ParkingLotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ParkingLotService {
+
+    private final ParkingLotRepository parkingLotRepository;
+
+    @Autowired
+    public ParkingLotService(ParkingLotRepository parkingLotRepository) {
+        this.parkingLotRepository = parkingLotRepository;
+    }
+
+    public ParkingLot getParkingLotInfo(Long id) {
+        Optional<ParkingLot> result = parkingLotRepository.findById(id);
+        return result.orElseThrow(() -> new IllegalStateException("잘못된 아이디"));
+    }
 }
