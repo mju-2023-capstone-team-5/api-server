81aae81f88afcc3d67958b1c14552f702e46d40a
terria1020 <jaehan1346@gmail.com>
2023-04-06 17:40:45 +0900

    feat: spring security 기본 설정 추가

---
+++ src/main/java/com/team5/capstone/mju/apiserver/web/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.team5.capstone.mju.apiserver.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and();
+        return http.build();
+    }
+
+}
