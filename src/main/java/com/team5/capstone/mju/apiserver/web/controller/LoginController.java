1ae9f4a9fff478b5ff7d14d973f49df47b3840fe
terria1020 <jaehan1346@gmail.com>
2023-04-15 03:57:07 +0900

    feat:  로그인(토큰 validate & 신규 유저 확인, jwt 생성) api 기초 틀 설계

--- src/main/java/com/team5/capstone/mju/apiserver/web/controller/LoginController.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/controller/LoginController.java
@@ -1,15 +1,27 @@
 package com.team5.capstone.mju.apiserver.web.controller;
 
+import com.team5.capstone.mju.apiserver.web.dto.LoginRequestDto;
+import com.team5.capstone.mju.apiserver.web.dto.LoginResponseDto;
 import com.team5.capstone.mju.apiserver.web.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class LoginController {
     private final LoginService loginService;
 
     @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
+    }
+
+    @PostMapping("/api/v1/login/token")
+    public ResponseEntity<LoginResponseDto> validationAndGenerateUserAndJwt(@RequestBody LoginRequestDto loginRequestDto) {
+        return ResponseEntity.ok(loginService.tryLogin(loginRequestDto));
     }
 }
--- src/main/java/com/team5/capstone/mju/apiserver/web/service/LoginService.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/service/LoginService.java
@@ -1,7 +1,32 @@
 package com.team5.capstone.mju.apiserver.web.service;
 
+import com.team5.capstone.mju.apiserver.web.dto.LoginRequestDto;
+import com.team5.capstone.mju.apiserver.web.dto.LoginResponseDto;
+import com.team5.capstone.mju.apiserver.web.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
+
+    private final UserRepository userRepository;
+
+    public LoginService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean isValidateToken() {
+        return false;
+    }
+
+    public boolean createOrFound() {
+        return true;
+    }
+
+    public LoginResponseDto tryLogin(LoginRequestDto loginRequestDto) {
+        return LoginResponseDto.builder()
+                .jwt(null)
+                .userId(null)
+                .isNewUser(true)
+                .build();
+    }
 }
