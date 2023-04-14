cde26645ad6ad8197d4a29a2a309b7eabafd7ae2
terria1020 <jaehan1346@gmail.com>
2023-04-14 16:56:20 +0900

    feat: 디비 수정으로 인한 User 엔티티의 일부 속성명 변경, User 엔티티 빌더 추가
    - 로그인(회원가입)에 알맞는 값을 저장하기 위한 속성명 변경
    -- 소셜로그인타입 -> kakao developers에서 부여 받은 자체 아이디 값
    -- 소셜 로그인 토큰 -> 소셜 로그인 refresh 토큰
    - 빌더 패턴 추가

--- src/main/java/com/team5/capstone/mju/apiserver/web/entity/User.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/entity/User.java
@@ -1,9 +1,6 @@
 package com.team5.capstone.mju.apiserver.web.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,6 +8,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "user")
@@ -32,11 +30,11 @@ public class User {
     @Column(name = "address")
     private String address;
 
-    @Column(name = "social_login_type")
-    private String socialLoginType;
+    @Column(name = "kakao_app_id", columnDefinition = "bigint")
+    private Long kakaoAppId;
 
-    @Column(name = "social_login_token")
-    private String socialLoginToken;
+    @Column(name = "social_login_refresh_token")
+    private String socialLoginRefreshToken;
 
     @Column(name = "car_number")
     private String carNumber;
