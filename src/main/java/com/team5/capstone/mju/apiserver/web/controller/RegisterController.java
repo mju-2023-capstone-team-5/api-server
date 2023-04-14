d20c8d1b56d1e5a9a64da3e935d6a3293121407d
terria1020 <jaehan1346@gmail.com>
2023-04-15 03:57:35 +0900

    feat: 회원가입(추가 정보 기입) api 기초 틀 설계

--- src/main/java/com/team5/capstone/mju/apiserver/web/controller/RegisterController.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/controller/RegisterController.java
@@ -1,7 +1,11 @@
 package com.team5.capstone.mju.apiserver.web.controller;
 
+import com.team5.capstone.mju.apiserver.web.dto.RegisterRequestDto;
 import com.team5.capstone.mju.apiserver.web.service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +15,11 @@ public class RegisterController {
     @Autowired
     public RegisterController(RegisterService registerService) {
         this.registerService = registerService;
+    }
+
+    @PostMapping("/api/v1/register")
+    public ResponseEntity<String> addNewInfo(@RequestBody RegisterRequestDto registerRequestDto) {
+        registerService.registerNewUser(registerRequestDto);
+        return ResponseEntity.ok("success");
     }
 }
--- src/main/java/com/team5/capstone/mju/apiserver/web/service/RegisterService.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/service/RegisterService.java
@@ -1,7 +1,18 @@
 package com.team5.capstone.mju.apiserver.web.service;
 
+import com.team5.capstone.mju.apiserver.web.dto.RegisterRequestDto;
+import com.team5.capstone.mju.apiserver.web.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterService {
+    private final UserRepository userRepository;
+
+    public RegisterService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void registerNewUser(RegisterRequestDto registerRequestDto) {
+        return;
+    }
 }
