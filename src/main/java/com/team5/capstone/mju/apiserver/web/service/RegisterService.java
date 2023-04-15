f6add71409092459fa7d0be0f4a94ee634ce898a
terria1020 <jaehan1346@gmail.com>
2023-04-16 01:54:36 +0900

    feat: 추가 정보 삽입하는 API 구현

--- src/main/java/com/team5/capstone/mju/apiserver/web/service/RegisterService.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/service/RegisterService.java
@@ -1,8 +1,12 @@
 package com.team5.capstone.mju.apiserver.web.service;
 
 import com.team5.capstone.mju.apiserver.web.dto.RegisterRequestDto;
+import com.team5.capstone.mju.apiserver.web.entity.User;
 import com.team5.capstone.mju.apiserver.web.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class RegisterService {
@@ -12,7 +16,14 @@ public class RegisterService {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void registerNewUser(RegisterRequestDto registerRequestDto) {
+        Optional<User> found = userRepository.findById(registerRequestDto.getUserId());
+
+        if (found.isEmpty()) throw new RuntimeException();
+
+        User user = found.get();
+        user.addExtraInfoSelf(registerRequestDto); // User 객체 스스로 추가 정보 삽입
         return;
     }
 }
