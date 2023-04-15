a07b3c6f9f28ccd06d775fb863fd6bbad021938a
terria1020 <jaehan1346@gmail.com>
2023-04-16 00:43:54 +0900

    feat: accessToken 검증 메소드 추가
    feat: accessToken 검증 메소드 추가
    - 액세스토큰이 잘못된 값이거나 expired 되었을 시 401 에러, JsonNode 해석 실패 시 에러, appId가 디벨로퍼스 앱 아이다와 다를 경우 에러, 그 외에 true로 액세스토큰 검증

--- src/main/java/com/team5/capstone/mju/apiserver/web/service/LoginService.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/service/LoginService.java
@@ -1,12 +1,34 @@
 package com.team5.capstone.mju.apiserver.web.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team5.capstone.mju.apiserver.web.dto.LoginRequestDto;
 import com.team5.capstone.mju.apiserver.web.dto.LoginResponseDto;
 import com.team5.capstone.mju.apiserver.web.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class LoginService {
+
+    @Value("${extends.app-id}")
+    private String appId;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private final UserRepository userRepository;
 
@@ -14,7 +36,27 @@ public class LoginService {
         this.userRepository = userRepository;
     }
 
-    public boolean isValidateToken() {
+    public boolean isValidateToken(LoginRequestDto loginRequestDto) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + loginRequestDto.getAccessToken());
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        JsonNode node = null;
+
+        try {
+            ResponseEntity<String> responseEntity = restTemplate.exchange(
+                    "https://kapi.kakao.com/v1/user/access_token_info",
+                    HttpMethod.GET,
+                    httpEntity,
+                    String.class
+            );
+            node = objectMapper.readTree(responseEntity.getBody());
+
+            if (node.get("app_id").asText().equals(appId)) return true;
+        } catch (HttpClientErrorException | JsonProcessingException e) { // json 해석 오류 / expired 되거나 잘못 된 token일 시(401)
+            return false;
+        }
         return false;
     }
 
@@ -23,6 +65,10 @@ public class LoginService {
     }
 
     public LoginResponseDto tryLogin(LoginRequestDto loginRequestDto) {
+
+        boolean isValidate = isValidateToken(loginRequestDto);
+        log.info("validate: " + isValidate);
+
         return LoginResponseDto.builder()
                 .jwt(null)
                 .userId(null)
