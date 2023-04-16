be302e6833001d33665c3dcbd88a74f1a9f8b3fa
terria1020 <jaehan1346@gmail.com>
2023-04-16 12:39:43 +0900

    fix: swagger(springdoc) https 관련 설정 추가로 요청오류 해결

--- src/main/java/com/team5/capstone/mju/apiserver/web/config/SpringConfig.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/config/SpringConfig.java
@@ -1,10 +1,15 @@
 package com.team5.capstone.mju.apiserver.web.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@OpenAPIDefinition(servers = {
+        @Server(url = "https://mju-2023capstone-team5.run.goorm.site")
+})
 public class SpringConfig {
 
     @Bean
