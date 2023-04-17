56d8c426f5ddbc35546dc051e8115661553babed
terria1020 <jaehan1346@gmail.com>
2023-04-17 15:54:32 +0900

    feat: 스프링부트의 환경 프로필 분리 및 SpringConfig 파일의 설정을 환경별로 분리

--- src/main/java/com/team5/capstone/mju/apiserver/web/config/ServerSpringConfig.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/config/ServerSpringConfig.java
@@ -4,11 +4,15 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class SpringConfig {
-
+@Profile("server")
+@OpenAPIDefinition(servers = {
+        @Server(url = "https://mju-2023capstone-team5.run.goorm.site")
+})
+public class ServerSpringConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
