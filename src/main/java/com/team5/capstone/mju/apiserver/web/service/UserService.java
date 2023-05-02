74475561af39d6db8b67a5f9ab77a786cdbd3336
terria1020 <jaehan1346@gmail.com>
2023-05-02 19:27:08 +0900

    feat: 회원탈퇴 후 후처리 API 구현
    회원탈퇴 후 후처리 API 구현
    - SDK에서 회원탈퇴 후 사용자 정보를 삭제하는 후처리 API 구현
    - 앱이 아닌 카카오 계정 관리 페이지나 고객센터에서 회원탈퇴 후 redirect로 사용자 정보를 삭제하게 하는 후처리 API 구현

--- src/main/java/com/team5/capstone/mju/apiserver/web/service/UserService.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/service/UserService.java
@@ -1,7 +1,11 @@
 package com.team5.capstone.mju.apiserver.web.service;
 
+import com.team5.capstone.mju.apiserver.web.entity.User;
 import com.team5.capstone.mju.apiserver.web.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -10,5 +14,22 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    // 사용자를 삭제하는 메소드
+    @Transactional
+    public void deleteUser(Long id) {
+        User found = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("해당하는 유저가 없습니다."));
+        userRepository.delete(found);
+    }
+
+    // 사용자를 삭제하는 메소드
+    // 카카오 계정 관리 페이지나 고객센터에서 연결끊기를 진행 시 넘어오는 정보는 kakaoId이기 때문에 kakaoId를 이용한 삭제처리
+    @Transactional
+    public void deleteUserFromKakaoId(Long kakaoId) {
+        User found = userRepository.findByKakaoAppUuid(String.valueOf(kakaoId))
+                .orElseThrow(() -> new EntityNotFoundException("해당하는 카카오 유저가 없습니다."));
+        userRepository.delete(found);
     }
 }
