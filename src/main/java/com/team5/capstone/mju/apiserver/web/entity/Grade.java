36d329db30582c37df12ee91e79b2ab27a9ed88e
terria1020 <jaehan1346@gmail.com>
2023-05-12 10:36:01 +0900

    fix: 참조 불가능한 객체 import문 삭제

--- src/main/java/com/team5/capstone/mju/apiserver/web/entity/Grade.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/entity/Grade.java
@@ -1,7 +1,6 @@
 package com.team5.capstone.mju.apiserver.web.entity;
 
 import com.team5.capstone.mju.apiserver.web.dto.GradeRequestDto;
-import com.team5.capstone.mju.apiserver.web.dto.ParkingLotRequestDto;
 import lombok.Getter;
 import lombok.Setter;
 
