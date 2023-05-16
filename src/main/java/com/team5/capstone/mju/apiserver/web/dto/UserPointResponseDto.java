4f99f37e02e0f14aca1e6de2ff029c32f49f9d29
terria1020 <jaehan1346@gmail.com>
2023-05-16 16:57:20 +0900

    feat: UserPoint entity로 UserPointResponseDTO 객체 생성을 해 주는 메소드 추가

--- src/main/java/com/team5/capstone/mju/apiserver/web/dto/UserPointResponseDto.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/dto/UserPointResponseDto.java
@@ -1,5 +1,6 @@
 package com.team5.capstone.mju.apiserver.web.dto;
 
+import com.team5.capstone.mju.apiserver.web.entity.UserPoint;
 import lombok.*;
 
 @Getter
@@ -11,4 +12,12 @@ public class UserPointResponseDto {
     private Integer userPointId;
     private Integer userId;
     private Long points;
+
+    public static UserPointResponseDto of(UserPoint userPoint) {
+        return UserPointResponseDto.builder()
+                .userPointId(Math.toIntExact(userPoint.getUserPointId()))
+                .userId(userPoint.getUserId())
+                .points(userPoint.getPoints())
+                .build();
+    }
 }
