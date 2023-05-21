afc292eaf893cc11f6033592ca6d6c561fc0e334
terria1020 <jaehan1346@gmail.com>
2023-05-21 13:42:53 +0900

    feat: 서비스 메소드에서 Firebase 관련 메소드 호출에 필요한 Firebase Admin key 파일 설정 추가, 그에 따른 키파일 경로 환경변수 추가

---
+++ src/main/java/com/team5/capstone/mju/apiserver/web/config/FireBaseConfig.java
@@ -0,0 +1,39 @@
+package com.team5.capstone.mju.apiserver.web.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Configuration
+public class FireBaseConfig {
+
+    @Value("${extends.firebase.admin-key-path}")
+    private String adminKeyPath;
+
+    public void firebaseConfiguration() {
+        FileInputStream serviceAccount =
+                null;
+        try {
+            serviceAccount = new FileInputStream(adminKeyPath);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        FirebaseOptions options = null;
+        try {
+            options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        FirebaseApp.initializeApp(options);
+    }
+}
