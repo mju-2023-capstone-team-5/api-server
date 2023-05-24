97df18e6cfc976a04896cbf86ab31be7bb3ef14e
terria1020 <jaehan1346@gmail.com>
2023-05-24 12:24:33 +0900

    fix: firebase설정 객체의 중복 초기화를 방지하기 위해 @Bean 어노테이션을 붙여 java bean으로 관리하게 설정

--- src/main/java/com/team5/capstone/mju/apiserver/web/config/FireBaseConfig.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/config/FireBaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
@@ -16,9 +17,11 @@ public class FireBaseConfig {
     @Value("${extends.firebase.admin-key-path}")
     private String adminKeyPath;
 
+    @Bean
     public void firebaseConfiguration() {
         FileInputStream serviceAccount =
                 null;
+
         try {
             serviceAccount = new FileInputStream(adminKeyPath);
         } catch (FileNotFoundException e) {
