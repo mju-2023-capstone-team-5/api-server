6eb2b3b3e1315f4587441b6ae8db489b8dd06d61
terria1020 <jaehan1346@gmail.com>
2023-05-30 21:31:00 +0900

    feat: 예약 시간이 다 됐을 시에 1회만 remaining_space를 증가시키고, 예약 내역은 삭제하지 않게 변경

--- src/main/java/com/team5/capstone/mju/apiserver/web/repository/ReservationRepository.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/repository/ReservationRepository.java
@@ -3,5 +3,8 @@ package com.team5.capstone.mju.apiserver.web.repository;
 import com.team5.capstone.mju.apiserver.web.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findAllByIsReturned(Boolean isReturned);
 }
