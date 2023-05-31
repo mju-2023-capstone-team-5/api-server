88746566baeca76ebbbf5e7c481fcb55e1eea0c2
terria1020 <jaehan1346@gmail.com>
2023-05-31 17:27:08 +0900

    feat: history 반환 객체에 isReviewWritten 추가, 그에 따른 객체 생성 패턴 수정

--- src/main/java/com/team5/capstone/mju/apiserver/web/dto/HistoryResponseDto.java
+++ src/main/java/com/team5/capstone/mju/apiserver/web/dto/HistoryResponseDto.java
@@ -2,6 +2,7 @@ package com.team5.capstone.mju.apiserver.web.dto;
 
 import com.team5.capstone.mju.apiserver.web.entity.History;
 import com.team5.capstone.mju.apiserver.web.entity.ParkingLot;
+import com.team5.capstone.mju.apiserver.web.entity.Reservation;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -17,13 +18,15 @@ public class HistoryResponseDto {
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private String parkingLotName;
+    private boolean isReviewWritten;
 
-    public static HistoryResponseDto of(History history, ParkingLot parkingLot) {
+    public static HistoryResponseDto of(History history, ParkingLot parkingLot, Reservation reservation) {
         return HistoryResponseDto.builder()
                 .reservationId(Long.valueOf(history.getReservationId()))
                 .startTime(history.getStartTime())
                 .endTime(history.getEndTime())
                 .parkingLotName(parkingLot.getName())
+                .isReviewWritten(reservation.getIsReviewWritten())
                 .build();
     }
 }
